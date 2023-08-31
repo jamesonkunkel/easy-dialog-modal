f389ecaee7adcb4dc285c4b2496ed52b60c8c79d
jamesonkunkel <jameson.kunkel@gmail.com>
2023-08-30 22:35:34 -0400

    Update index.test.tsx

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,2 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Modal } from '../src/index';
+
+describe('Modal', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Modal id="test_modal">
+        <p>I am test content.</p>
+      </Modal>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
