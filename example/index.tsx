181e0a731a0c4565751c9baa95d8ac1d53f9cfce
jamesonkunkel <jameson.kunkel@gmail.com>
2023-08-30 21:51:47 -0400

    Delete workflows files.

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,10 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+
 // import { Thing } from '../.';
 import { Modal } from '../src/index';
 
@@ -12,4 +16,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+root.render(<App />);
