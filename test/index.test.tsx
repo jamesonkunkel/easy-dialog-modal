c51b1b21f36ac6cfa787b41f28c59cba5fdf7a8b
jamesonkunkel <jameson.kunkel@gmail.com>
2023-08-31 15:36:21 -0400

    update test suite

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Modal } from '../src/index';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Modal } from "../src/index";
 
-describe('Modal', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("Modal", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(
       <Modal id="test_modal">
         <p>I am test content.</p>
